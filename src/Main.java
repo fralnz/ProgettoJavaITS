ee69dcfd09e3d83d45888bdba4e3bb34a170dbe0
FrancescoLanza <francesco@fragiolanza.eu>
2024-02-07 17:08:14 +0100

    implementazione di arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
+import static umani.Avventuriero.*;
+
 import umani.Avventuriero;
 import umani.Barbaro;
 import umani.Druido;
 import umani.Mago;
 
+import java.util.ArrayList;
 import java.util.Scanner;
-import static umani.Avventuriero.partyInit;
 
 /*
 Traccia: RPG
@@ -13,19 +15,13 @@ public class Main {
     public static void main(String[] args) {
         // inizializzo le variabili
         int giorno = 1;     //inizializzo i giorni
+        int membriParty = 3;
         Scanner tastiera = new Scanner(System.in);  //definisco un nuovo scanner
+        ArrayList<Avventuriero> party = new ArrayList<Avventuriero>();
         // messaggio di benvenuto
-        System.out.println("Benvenuto!");
         System.out.println("Inserisci tre avventurieri:");
         // Il tipo statico è Avventuriero
-        Avventuriero membro1 = partyInit();
-        System.out.println(membro1);
-        /*
-        Avventuriero membro2 = partyInit();
-        Avventuriero membro3 = partyInit();
-        System.out.println(membro2);
-        System.out.println(membro3);
-
-         */
+        umani.Avventuriero.partyInit(party, membriParty);
+        System.out.println(party);
     }
 }
--- src/umani/Avventuriero.java
+++ src/umani/Avventuriero.java
@@ -1,5 +1,6 @@
 package umani;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public abstract class Avventuriero {
@@ -32,55 +33,55 @@ public abstract class Avventuriero {
 
     public Avventuriero() {
     }
-    public static Avventuriero partyInit(){
+    public static void partyInit(ArrayList<Avventuriero> party, int membriParty) {
         Scanner tastiera = new Scanner(System.in);  //definisco un nuovo scanner
-        System.out.print("Inserisci il nome: ");
-        String nome = tastiera.nextLine();
-        System.out.print("Inserisci la razza: ");
-        String razza = tastiera.nextLine();
-        System.out.print("""
-                Inserisci il sesso (m/f)
-                (facoltativo)
-                """);
-        char sesso;
-        String sessoInp = tastiera.nextLine();
-        if (!sessoInp.equalsIgnoreCase("m") && !sessoInp.equalsIgnoreCase("f")){
-            sesso = 'X';
-        } else sesso = sessoInp.toUpperCase().charAt(0);
-        // controllo che il carattere inserito sia accettato
-        System.out.print("Inserisci l'eta (facoltativo): ");
-        int eta = tastiera.nextInt();      // da implementare la gesitione delle eccezioni per renderla effettivamente facoltativa
-        boolean ripeti;
-        do {
-            ripeti = false;
+        for (int i = 0; i < membriParty; i++) {
+            System.out.print("Inserisci il nome: ");
+            String nome = tastiera.nextLine();
+            System.out.print("Inserisci la razza: ");
+            String razza = tastiera.nextLine();
             System.out.print("""
-                    Inserisci la classe:
-                    0) Aiuto
-                    1) Barbaro
-                    2) Mago
-                    3) Druido
-                    4) Domatore
+                    Inserisci il sesso (m/f)
+                    (facoltativo)
                     """);
-            int scelta = tastiera.nextInt();
-            if (scelta == 0){
-                System.out.println("""
-                        1) BARBARO: dotato di grande forza e resistenza
-                        2) MAGO: capace di usare magia
-                        3) DRUIDO: capace di curare e parlare con gli animali
-                        4) DOMATORE: capace di addomesticare un animale e parlare con gli animali
-                        ...
+            char sesso;
+            String sessoInp = tastiera.nextLine();
+            if (!sessoInp.equalsIgnoreCase("m") && !sessoInp.equalsIgnoreCase("f")) {
+                sesso = 'X';
+            } else sesso = sessoInp.toUpperCase().charAt(0);
+            // controllo che il carattere inserito sia accettato
+            System.out.print("Inserisci l'eta (facoltativo): ");
+            int eta = tastiera.nextInt();      // da implementare la gesitione delle eccezioni per renderla effettivamente facoltativa
+            int scelta = 0;
+            do {
+                System.out.print("""
+                        Inserisci la classe:
+                        0) Aiuto
+                        1) Barbaro
+                        2) Mago
+                        3) Druido
+                        4) Domatore
                         """);
-                ripeti = true;
-            }
-            // Tipo Statico: Avventuriero
-            else if (scelta == 1) {
-                return new Barbaro(nome, razza, sesso, eta);
-            } else if (scelta == 2) {
-                return new Mago(nome, razza, sesso, eta);
-            } else if (scelta == 3) {
-                return new Druido(nome, razza, sesso, eta);
-            }
-        } while (ripeti);
-        return new Barbaro();
+                scelta = tastiera.nextInt();
+                if (scelta == 0) {
+                    System.out.println("""
+                            1) BARBARO: dotato di grande forza e resistenza
+                            2) MAGO: capace di usare magia
+                            3) DRUIDO: capace di curare e parlare con gli animali
+                            4) DOMATORE: capace di addomesticare un animale e parlare con gli animali
+                            ...
+                            """);
+                }
+                // Tipo Statico: Avventuriero
+                else if (scelta == 1) {
+                    party.add(new Barbaro(nome, razza, sesso, eta));
+                } else if (scelta == 2) {
+                    party.add(new Mago(nome, razza, sesso, eta));
+                } else if (scelta == 3) {
+                    party.add(new Druido(nome, razza, sesso, eta));
+                }
+            } while (scelta < 1 || scelta > 4);
+        }
+        tastiera.nextLine();
     }
 }
